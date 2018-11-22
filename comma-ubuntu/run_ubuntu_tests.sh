bc672daf7f51e4d7991e9010cf6bab0420a00873
bccw <513848839@qq.com>
2018-11-22 12:15:54 +0800

    comma-ubuntu

--- comma-ubuntu/run_ubuntu_tests.sh
+++ comma-ubuntu/run_ubuntu_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export PYTHONUNBUFFERED=1
 
+sudo apt-get update && apt-get install -y build-essential clang vim screen wget bzip2 git libglib2.0-0 python-pip capnproto libcapnp-dev libzmq5-dev libffi-dev libusb-1.0-0
+sudo pip install numpy==1.11.2 scipy==0.18.1 matplotlib
+
 cp requirements_openpilot.txt /tmp/
 pip install -r /tmp/requirements_openpilot.txt
 
